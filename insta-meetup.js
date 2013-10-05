8027597c38ec40e4d883e111bbb7d62a5a09a1b8
Quanquan Liu <qliu2011@gmail.com>
2013-10-05 17:11:04 -0400

    adding deleting by timestamp

--- insta-meetup.js
+++ insta-meetup.js
@@ -1,9 +1,21 @@
 var Meetups = new Meteor.Collection("meetups");
+var curDate = new Date();
 
 if (Meteor.isClient) {
   window.Meetups = Meetups;
-  Meteor.loginWithFacebook();
 
+  function deleteOld() {
+    var meets = Meetups.find({});
+    meets.forEach(function(meet) {
+          if(curDate.getDate() > (parseInt(meet.timestamp)+3) % 30) {
+            Meetups.remove({_id: meet._id});
+          }
+    });
+  }
+
+  deleteOld();
+
+  Meteor.loginWithFacebook();
   Template.feed.meetups = function() {
     return Meetups.find({}).fetch().reverse();
   };
@@ -11,8 +23,15 @@ if (Meteor.isClient) {
   Template.feed.events({
     'submit #new_meetup_wrapper': function() {
       var $meetup = $('#new_meetup');
-      Meetups.insert({meetup: $meetup.val()});
-      //$meetup.val() = '';
+      var datetime = "Creation Date: " + (curDate.getMonth()+1) + "/"
+                      + (curDate.getDate())  + "/"
+                      + curDate.getFullYear() + " @ "
+                      + curDate.getHours() + ":"
+                      + curDate.getMinutes() + ":"
+                      + curDate.getSeconds();
+      Meetups.insert({meetup: $meetup.val(), timestamp: new Date().getDate(), datetime: datetime});
+      $meetup.val('');
+      deleteOld();
       return false;
     }
   });
@@ -20,6 +39,6 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+  //server functions
   });
 }
